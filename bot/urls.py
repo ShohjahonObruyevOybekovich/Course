407ceb0bf6255e5e466d04f05725a7ef61830e1f
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-14 17:47:20 +0500

    example videos

--- bot/urls.py
+++ bot/urls.py
@@ -4,5 +4,5 @@ from .views import LeadCreateView, LandingPageView, telegram_webhook
 urlpatterns = [
     path("", LandingPageView.as_view(), name="landing"),
     path("leads/", LeadCreateView.as_view(), name="lead-create"),
-    path("webhook/",telegram_webhook),
+    path("bot/webhook/",telegram_webhook),
 ]
