da07e808decde1192800bcb60c1a240b52067f7e
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-14 18:24:53 +0500

    example videos

--- bot/management/commands/set_webhook.py
+++ bot/management/commands/set_webhook.py
@@ -4,12 +4,17 @@ from decouple import config
 from django.core.management.base import BaseCommand
 from aiogram import Bot
 from dispatcher import TOKEN
-BASE_URL = config('BASE_URL')
+
+BASE_URL = config("BASE_URL")
+
+
+async def set_webhook():
+    async with Bot(TOKEN) as bot:
+        webhook_url = f"{BASE_URL}/bot/webhook/"
+        await bot.set_webhook(webhook_url)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        bot = Bot(TOKEN)
-        webhook_url = f"{BASE_URL}/bot/webhook/"
-        asyncio.run(bot.set_webhook(webhook_url))
+        asyncio.run(set_webhook())
         self.stdout.write("Webhook set!")
